a3c18998d954bc7ac7622cce847b93552a8ef884
ChrisA <cteg516@gmail.com>
2019-09-12 18:41:11 -0700

    Revert "Merge branch 'mesay'"
    This reverts commit 0e8cebfbf16cfa44474db74384ce523098d90257, reversing
    changes made to a55c669158ede2fd9e3cadd0f53158e5619927a1.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,125 @@
 import React from "react";
-import Signup from "./Signup.js";
-import Signin from "./Signin.js";
-import Message from "./Message.js";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Signup from "./Signup";
+// import Signin from "./Signin";
+import Chatkit from '../node_modules/@pusher/chatkit-client/dist/web/chatkit'
+import MessageList from './components/messageList'
+import SendMessageForm from './components/sendmessageform'
+import RoomList from './components/roomlist'
+import NewRoomForm from './components/newroomform'
+import './App.css'
 
-// import Message from "./Message";
+import { tokenUrl, instanceLocator } from './config'
 
-function App() {
-  return (
-    <Router>
+class App extends React.Component {
+    
+  constructor() {
+      super()
+      this.state = {
+          roomId: null,
+          messages: [],
+          joinableRooms: [],
+          joinedRooms: []
+      }
+      this.sendMessage = this.sendMessage.bind(this)
+      this.subscribeToRoom = this.subscribeToRoom.bind(this)
+      this.getRooms = this.getRooms.bind(this)
+      this.createRoom = this.createRoom.bind(this)
+  } 
+  //chatmanager connection userId is a static value right now for testing purposes
+  componentDidMount() {
+      const chatManager = new Chatkit.ChatManager({
+          instanceLocator,
+          userId: '01',
+          tokenProvider: new Chatkit.TokenProvider({
+              url: tokenUrl
+          })
+      })
+      //sets current users connection, will need to be called for sign up or sign in
+      chatManager.connect()
+      .then(currentUser => {
+          this.currentUser = currentUser
+          this.getRooms()
+      })
+      .catch(err => console.log('error on connecting: ', err))
+  }
+  //displays current rooms that are joinable
+  getRooms() {
+      this.currentUser.getJoinableRooms()
+      .then(joinableRooms => {
+          this.setState({
+              joinableRooms,
+              joinedRooms: this.currentUser.rooms
+          })
+      })
+      .catch(err => console.log('error on joinableRooms: ', err))
+  }
+  //method for subscribing current user to a room
+  subscribeToRoom(roomId) {
+      this.setState({ messages: [] })
+      this.currentUser.subscribeToRoom({
+          roomId: roomId,
+          hooks: {
+              onNewMessage: message => {
+                  this.setState({
+                      messages: [...this.state.messages, message]
+                  })
+              }
+              
+          }
+      })
+      .then(room => {
+          this.setState({
+              roomId: room.id
+          })
+          this.getRooms()
+      })
+      .catch(err => console.log('error on subscribing to room: ', err))
+  }
+  //method for sending message, might need to parse the text to translate from here
+  sendMessage(text) {
+      this.currentUser.sendMessage({
+          text,
+          roomId: this.state.roomId
+      })
+  }
+  //method for creating a new chat room
+  createRoom(name) {
+      this.currentUser.createRoom({
+          name
+      })
+      .then(room => {
+          this.subscribeToRoom(room.id)
+      })
+      .catch(err => console.log('error with createRoom: ', err))
+  };
+ //changed sign in function to fit with my chatkit code. not sure if this is finished but i haven't messed with it yet. Edited it out for now since its unused and causes an error for me
+  /*Signin() {
+    return (
       <div>
-        {/* <NavTabs /> */}
-        {/* <Route exact path="/" component={Home} /> */}
-        <Route exact path="/SignUp" component={Signup} />
-        <Route exact path="/Signin" component={Signin} />
-        <Route exact path="/Message" component={Message} />
+        {/* <Signup /> }
+        <Signin />
       </div>
-    </Router>
-  );
+    );
+  } */
+  
+  
+  render() {
+      return (
+          <div className="app">
+              <RoomList
+                  subscribeToRoom={this.subscribeToRoom}
+                  rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]}
+                  roomId={this.state.roomId} />
+              <MessageList 
+                  roomId={this.state.roomId}
+                  messages={this.state.messages} />
+              <SendMessageForm
+                  disabled={!this.state.roomId}
+                  sendMessage={this.sendMessage} />
+              <NewRoomForm createRoom={this.createRoom} />
+          </div>
+      );
+  }
 }
 
 
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 300;
 
 const User = require("./models/user.js");
-const Message = require("./models/message");
 
 mongoose.connect('mongodb+srv://jessica:translation@cluster0-vfzwn.mongodb.net/test?retryWrites=true&w=majority', 
   {useNewUrlParser: true})
@@ -46,7 +45,6 @@ app.post("/signin", function(req, res) {
   console.log(req.body);
   User.findOne(req.body)
     .then(function(dbUser) {
-      console.log("Signed in");
       // If saved successfully, send the the new User document to the client
       res.json(dbUser);
     })
@@ -56,74 +54,6 @@ app.post("/signin", function(req, res) {
     });
 });
 
-app.get("/users", function(req, res) {
-  console.log("Do something");
-  User.find({})
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.get("/messages", function(req, res) {
-  console.log("Get messages");
-  Message.find({})
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.post("/send", function(req, res) {
-  console.log("Send message");
-  console.log(req.body);
-  Message.create(req.body)
-    .then(function(dbMessage) {
-      // If saved successfully, send the the new User document to the client
-      res.json(dbMessage);
-    })
-    .catch(function(err) {
-      // If an error occurs, send the error to the client
-      res.json(err);
-    });
-  //  Get the properties from the front end via req.body
-  //  Find a way to get the User ObjectId when a message is sent
-
-  //  Create a new message
-});
-
-app.get("/inbox", function(req, res) {
-  //  Retrieve a number of messages from a user utilizing the User ObjectId
-  //  Use .populate(<Schema property>) to get the actual name(s) to display
-});
-
-app.get("/", function(req, res) {
-  res.send("Routing");
-});
-
-mongoose
-  .connect(
-    "mongodb://Mesay:Mesi463946@cluster0-shard-00-00-updmb.mongodb.net:27017,cluster0-shard-00-01-updmb.mongodb.net:27017,cluster0-shard-00-02-updmb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
-    // "mongodb+srv://Mesay:Mesi463946@cluster0-updmb.mongodb.net/test?retryWrites=true&w=majority",
-    // process.env.MONGODB_URI ||
-    //   "mongodb://Mesay:Mesi463946@cluster0-shard-00-00-vfzwn.mongodb.net:27017,cluster0-shard-00-01-vfzwn.mongodb.net:27017,cluster0-shard-00-02-vfzwn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
-    // "mongodb+srv://Mesay:Mesi463946@cluster0-vfzwn.mongodb.net/test?retryWrites=true&w=majority",
-    // mongodb+srv:Mesay:<>@cluster0-vfzwn.mongodb.net/test?retryWrites=true&w=majority
-    // "mongodb://Mesay:Mesi463946@cluster0-updmb.mongodb.net/test?retryWrites=true&w=majority",
-    // "mongodb://Mesay:Mesi463946@cluster0-shard-00-00-updmb.mongodb.net:27017,cluster0-shard-00-01-updmb.mongodb.net:27017,cluster0-shard-00-02-updmb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
-    { useNewUrlParser: true }
-  )
-
-  .then(() => {
-    console.log("DB connected");
-  })
-  .catch(err => {
-    console.log(err);
-  });
 
 // Start the API server
 app.listen(PORT, function() {
