8999b77a669136c6d07335fd3960f3d5dff3a13d
MFTeg <48466052+MFTeg@users.noreply.github.com>
2019-09-12 18:44:53 -0700

    Delete config.js
    chatkit scrapped

--- client/src/config.js
+++
@@ -1,4 +0,0 @@
-const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/dffc202a-3ceb-4f73-a79b-054638e3a229/token";
-const instanceLocator = "v1:us1:dffc202a-3ceb-4f73-a79b-054638e3a229";
-
-export { tokenUrl, instanceLocator }
