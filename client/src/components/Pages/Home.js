ca49d933216d7dbb2dd3d91beba8782b69b39c16
MesayBeyene <mesibella@gmail.com>
2019-09-21 13:46:25 -0700

    css update

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import Vision from "../VisionStatment/Vision";
 // import LogoImg from "../components/Image/logoImg.png";
 // import basketOfKittens from "../image/basket-of-kittens.jpg";
 
@@ -11,6 +12,7 @@ class Search extends Component {
       <div>
         <Navbar />
         <Header />
+        <Vision />
         <Footer />
         {/* <img src="./image/cute-cat.jpeg" alt="Placeholder" /> */}
       </div>
