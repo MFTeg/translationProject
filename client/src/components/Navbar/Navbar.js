5d5a274c943185746e99364239ca58c904d65bd6
MesayBeyene <mesibella@gmail.com>
2019-09-29 23:05:00 -0700

    update on Sunday 09/29

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -14,43 +14,37 @@ function Navbar() {
         <a id="logo-container" href={"/"} className="brand-logo">
           Translation Station
         </a>
-        <ul className="right hide-on-med-and-down">
-          <li className="button">
-            <a
-              href={"/Signup"}
-              id="download-button"
-              className="btn-large waves-effect waves-light blue lighten-1"
-            >
-              Sign Up
+
+        {button}
+
+        <ul id="nav-mobile" className="sidenav">
+          <li className="divider">
+            <li>
+              <a className="sidenavClass" href={"/"}>
+                Home
+              </a>
+            </li>
+          </li>
+          <li className="divider">
+            <a className="sidenavClass" href={"/signin"}>
+              Sign In
             </a>
           </li>
-          <li>
-            <a
-              href={"/Signin"}
-              id="download-button"
-              className="btn-large waves-effect waves-light blue lighten-1"
-            >
-              Sign In{" "}
-            </a>{" "}
-          </li>
-          <li>
-            <a
-              href={"/Message"}
-              id="download-button"
-              className="btn-large waves-effect waves-light blue lighten-1"
-            >
-              Message
-            </a>
+
+          <li className="divider">
+            <a href={"/signup"}>Sign Up</a>
           </li>
         </ul>
 
-        <ul id="nav-mobile" className="sidenav">
-          <li>
-            <a href={"/"}>Navbar Link</a>
-          </li>
-        </ul>
-        <a href={"/"} data-target="nav-mobile" className="sidenav-trigger">
-          <i className="material-icons">menu</i>
+        <a
+          className="sidenavClass"
+          href={"/"}
+          data-target="nav-mobile"
+          className="sidenav-trigger"
+        >
+          <i className="material-icons" id="sideNavMenu">
+            &#xE5D2;
+          </i>
         </a>
       </div>
     </nav>
