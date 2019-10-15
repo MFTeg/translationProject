e3696cdd723cd77970b99ba637dd346186401178
MesayBeyene <mesibella@gmail.com>
2019-10-15 05:50:27 -0700

    fixing the routes on heroku

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,14 @@ import Message from "./components/Pages/Message";
 import Home from "./components/Pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-// import Message from "./Message";
-
-// import SignIn from './Signin'
-// import SignUp from './Signup'
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          {/* <NavTabs /> */}
           <Route exact path="/" component={Home} />
           <Route exact path="/Signup" component={Signup} />
           <Route exact path="/Signin" component={Signin} />
-          {/* <Route exact path="/Chat" component={Chat}/> */}
           <Route exact path="/Message" component={Message} />
         </Switch>
       </div>
